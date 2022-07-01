cd5a40cf7fa321824227f5441a2844aecefa8728
Haylo <javier.serrano04@estudiant.upf.edu>
2022-07-01 02:24:30 +0200

    Final Version

--- src/game.cpp
+++ src/game.cpp
@@ -58,6 +58,12 @@ Game::Game(int window_width, int window_height, SDL_Window* window)
 	//Load the scene JSON
 	if (!scene->load("data/scene.json"))
 		exit(1);
+
+	for (int i = 0; i < scene->objects.size(); i++)
+	{
+		ObjectEntity* object = scene->objects[i];
+
+	}
 	
 	//Create an entity editor
 	entity_editor = new Editor3D(scene);
